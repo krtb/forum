e688c88479c43993782680b91107d5c4b205945b
Kurt Bauer <elkrtb@pm.me>
2025-02-17 14:59:16 -0500

    add eslint rules for no-console and no-debugger

--- eslint.config.js
+++ eslint.config.js
@@ -20,7 +20,9 @@ export default [
   {
     name: 'app/custom-rules',
     rules: {
-      'no-unused-vars': process.env.NODE_ENV === 'production' ? 'error' : 'warn'
+      'no-unused-vars': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+      'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
     }
   }
 ]
